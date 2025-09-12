8ddb94de27cf2e6d2c209a61f00a3d3d9f1df323
leo.logel <leo.logel@sdea.fr>
2025-09-12 13:23:51 +0200

    feat : set client call code base (with mlflow tracking call)

--- mlflow/config_tracking.py
+++
@@ -1,6 +0,0 @@
-import mlflow
-
-mlflow.openai.autolog()
-
-mlflow.set_tracking_uri("http://localhost:5000")
-mlflow.set_experiment("RAG-llama")
---
+++ src/ollama/client.py
@@ -0,0 +1,38 @@
+from openai import OpenAI
+import mlflow
+
+# https://mlflow.org/docs/latest/genai/tracing/integrations/listing/ollama/
+
+OllamaClient = OpenAI(
+    base_url="http://localhost:11435/v1",
+    api_key=""
+)
+
+def set_mlflow_tracking(experiment_name: str):
+    import mlflow
+
+    mlflow.openai.autolog()
+
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment(experiment_name)
+
+def get_token_usage(trace_id = None):
+    if trace_id is None:
+        trace_id = mlflow.get_last_active_trace_id()
+
+    trace = mlflow.get_trace(trace_id)
+
+    total_usage = trace.info.token_usage
+    print("== Total token usage: ==")
+    print(f"  Input tokens: {total_usage['input_tokens']}")
+    print(f"  Output tokens: {total_usage['output_tokens']}")
+    print(f"  Total tokens: {total_usage['total_tokens']}")
+
+    # Print the token usage for each LLM call
+    print("\n== Detailed usage for each LLM call: ==")
+    for span in trace.data.spans:
+        if usage := span.get_attribute("mlflow.chat.tokenUsage"):
+            print(f"{span.name}:")
+            print(f"  Input tokens: {usage['input_tokens']}")
+            print(f"  Output tokens: {usage['output_tokens']}")
+            print(f"  Total tokens: {usage['total_tokens']}")
